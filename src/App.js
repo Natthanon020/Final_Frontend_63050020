acfa0c7f56fbd4a4771003712ebfac2a21f500c3
Nathanon020 <nathanonchumi@gmail.com>
2023-10-30 11:46:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Box, Container } from "@mui/material";
+import React from "react";
+import Header from "./Components/Header";
+import List from "./Components/List";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box position="relative">
+      {/* <Header /> */}
+      <Container>
+        <Header/>
+        <List />
+      </Container>
+    </Box>
   );
-}
+};
 
 export default App;
